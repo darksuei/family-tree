2a58b7d666d69f283993ec2e1842b19df96f7562
Suei43 <chrisraphael576@gmail.com>
2023-07-09 17:52:06 +0100

    vercel img error

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static('./'));
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/', loginRouter);
