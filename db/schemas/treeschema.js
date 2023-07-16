99a49c2b30582ed9505904585b5988d1aa4ea876
Suei43 <chrisraphael576@gmail.com>
2023-07-16 17:10:24 +0100

    Working man: edit page updates

--- db/schemas/treeschema.js
+++ db/schemas/treeschema.js
@@ -6,11 +6,20 @@ const Schema = mongoose.Schema
 
 const treeSchema = new Schema(
     {
-        name: Schema.Types.Mixed,
-        parent: Schema.Types.Mixed,
-        married: Schema.Types.String,
-        spouse: Schema.Types.String,
-        children: Schema.Types.Mixed
+        // name: Schema.Types.Mixed,
+        // parent: Schema.Types.Mixed,
+        // married: Schema.Types.String,
+        // spouse: Schema.Types.String,
+        // children: Schema.Types.Mixed
+        email: Schema.Types.String,
+        fathersfirstname : Schema.Types.String,
+        fatherslastname : Schema.Types.String,
+        mothersfirstname : Schema.Types.String,
+        motherslastname : Schema.Types.String,
+        childfirstname : Schema.Types.String,
+        childlastname : Schema.Types.String,
+        childspousefirstname : Schema.Types.String,
+        childspouselastname : Schema.Types.String,
       }
 )
 module.exports = {treeSchema}
