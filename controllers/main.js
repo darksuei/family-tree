dd4d02b54903463a8ccaaac661f97a943f0c2a70
Suei43 <chrisraphael576@gmail.com>
2023-08-04 15:04:56 +0100

    Feat: Nesting logic update

--- controllers/main.js
+++ controllers/main.js
@@ -1,41 +1,6 @@
 let path = require('path')
 const Tree = require('../models/treemodel')
 
-//FUNCTION FOR NESTING THE INPUT IN JSON FORMAT
-const constructobj = (obj)=>{
-  const one = {}
-  let two = one
-  let counter = 0;
-  let defaultarr = [{},{},{},{},{}];
-  let holdarr = [{},{},{},{},{}];
-
-  while(obj.length>0 && obj[0]){
-    console.log(obj)
-    let arr = defaultarr;
-    let val = obj[0];
-    let val2 = obj[1];
-    obj.shift();
-    obj.shift();
-
-    if(counter == 0){
-      two.name = val;
-      two.parent = "root";
-      two.married = "+";
-      two.spouse = val2;
-      two.children = defaultarr;
-  }
-  else if(counter >=1){
-    arr[counter-1].name = val;
-    arr[counter-1].parent = two.name; //parent should inherit name from immediate parent node
-    arr[counter-1].married = "+";
-    arr[counter-1].spouse = val2;
-    arr[counter-1].children = [];
-  }
-    counter++;
-  }
-  return one;
-}
-
 module.exports.index = function(req, res) {
   let isloggedIn = false;
   if(req.session.data){
@@ -127,14 +92,7 @@ module.exports.editpost = function (req,res){
       "parent" : "root",
       "married" : "+",
       "spouse" : req.body.motherfname +" "+ req.body.motherlname,
-      "children" : [
-        {
-          "name" : req.body.childfname +" "+ req.body.childlname,
-          "parent" : req.body.fatherfname +" "+ req.body.fatherlname,
-          "married" : "+",
-          "spouse" : req.body.cspousefname +" "+ req.body.cspouselname
-        }
-      ]
+      "children" : createChildrenArray(req.body.fatherfname,req.body.fatherlname,req.body.child1fname,req.body.child1lname,req.body.child2fname,req.body.child2lname,req.body.child3fname,req.body.child3lname)
     }
     // let treeobj = constructobj(Object.values(req.query)) RECURSSION FUNCTION
 
@@ -153,25 +111,53 @@ module.exports.editputpost = function (req,res,next){
   next();
 }
 
-module.exports.editput = function (req, res) {
-  
+module.exports.editpost = function (req,res){
   let usertree = {
     "email" : req.session.data.user.email,
     "name" : req.body.fatherfname +" "+ req.body.fatherlname,
     "parent" : "root",
     "married" : "+",
     "spouse" : req.body.motherfname +" "+ req.body.motherlname,
-    "children" : [
-      {
-        "name" : req.body.childfname +" "+ req.body.childlname,
-        "parent" : req.body.fatherfname +" "+ req.body.fatherlname,
-        "married" : "+",
-        "spouse" : req.body.cspousefname +" "+ req.body.cspouselname
-      }
-    ]
+    "children" : createChildrenArray(req.body.fatherfname,req.body.fatherlname,req.body.child1fname,req.body.child1lname,req.body.child2fname,req.body.child2lname,req.body.child3fname,req.body.child3lname)
   }
+  // let treeobj = constructobj(Object.values(req.query)) RECURSSION FUNCTION
 
-    Tree.findOneAndUpdate({ email: req.session.data.user.email }, usertree, { new: true, useFindAndModify: false })
+Tree.create(usertree).then(() => {
+  res.status(200).redirect('/success');
+}).catch((error) => {
+  console.log(error)
+  res.status(400).send(error);
+})
+}
+
+module.exports.editputpost = function (req,res,next){
+if (req.headers['x-http-method-override'] === 'PUT') {
+  req.method = 'PUT';
+}
+next();
+}
+
+module.exports.editput = async function (req, res) {
+  //SETUP USER INPUT AS AN OBJECT
+  let usertree = {
+    "email" : req.session.data.user.email,
+    "name" : removeSpacesFromEnd(req.body.fatherfname) + " " + removeSpacesFromEnd(req.body.fatherlname),
+    "parent" : "root",
+    "married" : "+",
+    "spouse" : req.body.motherfname + " " + req.body.motherlname,
+    "children" : createChildrenArray(req.body.fatherfname, req.body.fatherlname, req.body.child1fname, req.body.child1lname, req.body.child2fname, req.body.child2lname, req.body.child3fname, req.body.child3lname)
+  };
+
+  //GET THE EXISTING USER TREE FROM DATABASE
+  var email = req.session.data.user.email;
+  var treeDetails = await Tree.findOne({ email: email }, { '_id': false, '__v': false });
+
+  //UPDATE THE USER TREE WITH THE NEW USER INPUT
+  var updatedTree = await updateTreeDetails(treeDetails, usertree);
+  console.log(updatedTree);
+
+  //UPDATE THE USER TREE IN THE DATABASE
+  Tree.findOneAndUpdate({ email: req.session.data.user.email }, updatedTree, { new: true, useFindAndModify: false })
     .then((updatedtree) => {
       if (updatedtree) {
         // Tree found and updated successfully
@@ -188,3 +174,57 @@ module.exports.editput = function (req, res) {
       res.status(500).send('An Error Occurred');
     });
 };
+
+
+function createChildrenObject(childfname,childlname,fatherfname,fatherlname){
+  return {
+    "name" : childfname +" "+ childlname,
+    "parent" : fatherfname +" "+ fatherlname,
+    "married" : "+",
+    "spouse" : " ",
+    "children" : []
+  }
+}
+
+function createChildrenArray(fatherfname, fatherlname, child1fname, child1lname, child2fname, child2lname, child3fname, child3lname){
+  let arr = [];
+  if (child1fname){
+    arr.push(createChildrenObject(child1fname,child1lname, fatherfname, fatherlname))
+  }
+  if (child2fname){
+    arr.push(createChildrenObject(child2fname,child2lname, fatherfname, fatherlname))
+  }
+  if (child3fname){
+    arr.push(createChildrenObject(child3fname,child3lname, fatherfname, fatherlname))
+  }
+  return arr;
+}
+
+function removeSpacesFromEnd(str){
+  while (str.charAt(str.length-1) === " "){
+    str = str.slice(0,-1);
+  }
+  return str;
+}
+
+async function updateTreeDetails(treeNode, usertree) {
+  if (treeNode["name"] === usertree["name"]) {
+    // If the names match, replace the entire node with usertree
+    return usertree;
+  } else {
+    if (treeNode.children && Array.isArray(treeNode.children)) {
+      for (let i = 0; i < treeNode.children.length; i++) {
+        // Recursively search for the name in the children
+        treeNode.children[i] = await updateTreeDetails(treeNode.children[i], usertree);
+        if (treeNode.children[i]["name"] === usertree["name"]) {
+          // If the names match after updating children, replace only the matching child with usertree
+          return treeNode;
+        }
+      }
+    }
+
+    // If the name is not found in the children, return the unchanged treeNode
+    return treeNode;
+  }
+}
+
